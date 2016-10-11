99127890719902c137d2e69df47ab41c1fa1066b
Mariano Montero <teromb93@gmail.com>
2016-10-10 20:18:53 -0600

    Added nav-bar
    Signed-off-by: Mariano Montero <teromb93@gmail.com>

--- creativeriaSpotify/src/app/creativeria-spotify.component.ts
+++ creativeriaSpotify/src/app/creativeria-spotify.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import {NavbarComponent} from './components/navbar/navbar.component';
 @Component({
   moduleId: module.id,
   selector: 'creativeria-spotify-app',
   templateUrl: 'creativeria-spotify.component.html',
-  styleUrls: ['creativeria-spotify.component.css']
+  styleUrls: ['creativeria-spotify.component.css'],
+  directives:[NavbarComponent]
 })
 export class CreativeriaSpotifyAppComponent {
   title = 'creativeria-spotify works!';
